626111ea1342f8e0a46acdc2b30d771aed2cf7e6
Moosa Baig <am.moosabaig@gmail.com>
2026-09-23 16:45:48 +0500

    finished

--- Lab5/Task2.c
+++ Lab5/Task2.c
@@ -19,7 +19,7 @@ int main(){     //Start main function
         printf("Do you have an existing loan? (Enter 1- True or 0- False): ");  //Inputs whether user has a loan or not
         scanf("%d", &existing_loan);
 
-        if(income>=100000 && credit_score >= 750 && existing_loan == 0)
+        if(income>=100000 && credit_score >= 750 && existing_loan == 0)     //Checks conditions, then prints varying chances of approval
             printf("High Approval Chance");
         else if(income>=75000 && credit_score >= 650 && existing_loan == 1)
             printf("Manual Review");
